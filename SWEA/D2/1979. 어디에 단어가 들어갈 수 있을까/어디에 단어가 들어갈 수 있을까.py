b4d94c86564647f5f9e02976c3feab475a495006
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2023-11-10 19:18:10 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 160 ms, Memory: 61,464 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -6,42 +6,31 @@ NxN 크기의 단어 퍼즐에서 특정 길이 K를 갖는 단어가 들어갈 
 풀이
 그래프 탐색 알고리즘을 굳이 쓸 필요는 없을 것 같다.
 '''
-#17:13 - 18:05
+#17:13 - 18:25
 
 t = int(input())
-
-for tc in range(1, t+1):
-    n, k = map(int, input().split())
-    puzzle = [list(map(int, input().split())) for _ in range(n)]
-    #가로 확인
+def calc(puzzle):
     ans = 0
     for i in range(n):
         cnt = 0
         for j in range(n):
             if puzzle[i][j] == 1:
                 cnt += 1
-            else: #수정한 부분
-                if cnt == k:
-                    ans += 1
-                    cnt = 0
-                else:
-                    cnt = 0
+            else:  # 0일 때, 만약 7x7 퍼즐에서 단어가 3자리일 때 가로에서 2가지가 나올 수 있기 때문
+                if cnt == k:  # 카운트값과 단어 자릿수랑 맞으면
+                    ans += 1  # 결과값에 1을 더하고
+                    cnt = 0  # 카운트값 초기화
+                else:  # 카운트값과 단어 자릿수랑 맞지 않으면
+                    cnt = 0  # 결과값은 무시하고, 카운트값 초기화
         if cnt == k:
             ans += 1
+    return ans
 
-    #세로 확인
-    for i in range(n):
-        cnt = 0
-        for j in range(n):
-            if puzzle[j][i] == 1:
-                cnt += 1
-            else: #수정한 부분
-                if cnt == k:
-                    ans += 1
-                    cnt = 0
-                else:
-                    cnt = 0
-        if cnt == k:
-            ans += 1
+for tc in range(1, t+1):
+    n, k = map(int, input().split())
+    puzzle = [list(map(int, input().split())) for _ in range(n)]
+    ans = 0
+    ans += calc(puzzle) #가로
+    ans += calc(list(zip(*puzzle))) #세로
     print('#'+str(tc), ans)
-#1차 제출 실패 - input 파일 중 6개만 맞았음,
+#1차 제출 실패 - input 파일 중 6개만 맞았음, 조건부 수정하고 2차 제출 후 성공
