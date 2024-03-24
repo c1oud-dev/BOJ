0f8c01468f5d0d10302c6f69ce920992fb6aeedb
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-03-24 20:53:20 +0900

    [Bronze III] Title: A+B - 6, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -1,17 +1,22 @@
-import java.io.*;
-import java.util.*;
+/*
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+A와 B는 콤마(,)로 구분되어 있다.
+#20:30 - 20:51
+*/
 
+import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt(); //테스트 케이스
         for (int i = 0; i < t; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(a+b + "\n");
+            String num = sc.next(); //nextLine -> next로 변경하니 에러 사라짐
+            char a = num.charAt(0);
+            char b = num.charAt(2);
+            int a1 = a - '0';
+            int b1 = b - '0';
+            System.out.println(Character.getNumericValue(a) + Character.getNumericValue(b));
         }
-        System.out.print(sb);
     }
 }
