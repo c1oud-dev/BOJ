1493418f0dd8003cee70a470f00d500a33d56a6b
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-09-28 21:41:35 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 176 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.java
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		String[] p = {"Never gonna give you up", "Never gonna let you down", "Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye", "Never gonna tell a lie and hurt you", "Never gonna stop"};
-		
-		int i, j;
-		int n = sc.nextInt();
-		String[] a = new String[n + 1];
-		String result = "Yes";
-		
-		for(i = 0; i < a.length; i++) {
-			a[i] = sc.nextLine();
-		}
-		
-		for(i = 1; i < a.length; i++) {
-			result = "Yes";
-			for(j = 0; j < p.length; j++) {
-				if(a[i].equals(p[j]) == true) {
-					result = "No";
-					break;
-				}
-			}
-			
-			if(result.equals("Yes")) {
-				break;
-			}
-		}
-		
-		System.out.println(result);
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(); // 정수 n 입력 받아서 저장
+
+        String[] pledge = {"Never gonna give you up", "Never gonna let you down",
+                "Never gonna run around and desert you", "Never gonna make you cry",
+                "Never gonna say goodbye", "Never gonna tell a lie and hurt you", "Never gonna stop"};
+
+        String[] translation = new String[num + 1]; //입력받은 정수만큼 배열을 생성
+        for (int i = 0; i < translation.length; i++) {
+            translation[i] = sc.nextLine(); //공약 입력받기
+        }
+
+        String result = "Yes";
+        
+        for (int i = 1; i < translation.length; i++) { //앞에 공백 때문에 i = 1 부터 시작
+            result = "Yes";
+            for (int j = 0; j < pledge.length; j++) {
+                if(translation[i].equals(pledge[j]) == true) {
+                    result = "No";
+                    break;
+                }
+            }
+            if (result.equals("Yes")) {
+                break;
+            }
+        }
+        System.out.println(result);
+        sc.close();
+    }
 }
