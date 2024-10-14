3575b8467389cfaaf5f495274503899891891918
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-14 16:25:34 +0900

    [Bronze III] Title: 네 번째 점, Time: 116 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -3,16 +3,22 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] r1 = new int[3];
-        int[] r2 = new int[3];
+        int[] x = new int[3];
+        int[] y = new int[3];
 
         for (int i = 0; i < 3; i++) {
             String[] nums = br.readLine().split(" ");
-            r1[i] = Integer.parseInt(nums[0]);
-            r2[i] = Integer.parseInt(nums[1]);
+            x[i] = Integer.parseInt(nums[0]);
+            y[i] = Integer.parseInt(nums[1]);
         }
-        int result1 = (r1[0] == r1[1]) ? r1[2] : (r1[0] == r1[2]) ? r1[1] : r1[0];
-        int result2 = (r2[0] == r2[1]) ? r2[2] : (r2[0] == r2[2]) ? r2[1] : r2[0];
+        int result1, result2;
+        if (x[0] == x[1]) result1 = x[2];
+        else if (x[0] == x[2]) result1 = x[1];
+        else result1 = x[0];
+
+        if (y[0] == y[1]) result2 = y[2];
+        else if (y[0] == y[2]) result2 = y[1];
+        else result2 = y[0];
 
         System.out.println(result1 + " " + result2);
     }
