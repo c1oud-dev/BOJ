d632969350e069eedece0cfecd64efd1e479cdaf
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-24 11:49:55 +0900

    [D3] Title: 농작물 수확하기, Time: 207 ms, Memory: 49,572 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,19 +1,23 @@
-T = int(input())
+'''
+n * n 크기의 농장이 있다. 다음과 같은 규칙이 있다.
+1. 농장의 크기는 항상 홀수이다.
+2. 수확은 항상 농장의 크기에 딱 맞는 정사각형 마름모 형태로만 가능하다.
+이렇게 해서 얻을 수 있는 수익은 얼마인지 구하여라.
+'''
+test_case = int(input())
+for tc in range(1, test_case + 1):
+    n = int(input()) #농장 크기
+    farm = [list(map(int, input())) for _ in range(n)] #정수이므로 리스트로 받기
+    result = 0
+    start, end = n // 2, n // 2
 
-for tc in range(1, T+1):
-    N = int(input())
-    a = [list(map(int, input())) for _ in range(N)]
-    ans = 0 
-
-    s, e = N // 2, N // 2
-    for i in range(N):
-        for j in range(s, e+1):
-            ans += a[i][j]
-        if i < N // 2:
-            s -= 1
-            e += 1
+    for i in range(n):
+        for j in range(start, end + 1):
+            result += farm[i][j]
+        if i < n // 2:
+            start -= 1
+            end += 1
         else:
-            s += 1
-            e -= 1
-
-    print("#{} {}".format(tc, ans))
+            start += 1
+            end -= 1
+    print("#%d %d" %(tc, result))
