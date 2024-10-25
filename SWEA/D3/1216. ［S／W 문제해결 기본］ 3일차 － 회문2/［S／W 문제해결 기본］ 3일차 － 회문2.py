9d2ca53e522af60383ab2a2cc3716d81f1dbbc90
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-25 12:56:43 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 2,938 ms, Memory: 59,456 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -2,17 +2,16 @@
 100 * 100 평면 글자판에서 가로, 세로를 모두 보아 가장 긴 회문의 길이를 구하는 문제
 '''
 def palindrome(board):
-    length, max_len = 0, 0
+    max_len = 0
     for i in range(100):
         for j in range(100):
-            for k in range(100):
-                word_slice = board[i][j:j + k]
+            for k in range(100, 0, -1): #큰 길이부터 시작하여 검사
+                if j + k <= 100:
+                    word_slice = board[i][j:j + k]
 
-                if word_slice == word_slice[::-1]:
-                    length = len(word_slice)
-
-                if max_len < length:
-                    max_len = length
+                    if word_slice == word_slice[::-1]: # 회문 확인
+                        max_len = max(max_len, k) # 최대 길이 갱신
+                        break # 최대 길이 찾았으므로 더 짧은 길이는 검사하지 않음
     return max_len
 
 for _ in range(10):
@@ -22,9 +21,6 @@ for _ in range(10):
     #행 열 변환
     new_board = [list(i) for i in zip(*board)]
     col_len = palindrome(new_board)
-    if row_len > col_len:
-        result = row_len
-    else:
-        result = col_len
+    result = max(row_len, col_len)
 
     print(f"#{test_case} {result}")
