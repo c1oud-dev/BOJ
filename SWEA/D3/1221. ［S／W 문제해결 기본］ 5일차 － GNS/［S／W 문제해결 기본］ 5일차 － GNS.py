e5e82fc823b11248695bf3d24d0ac1778ab2a017
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-26 14:57:36 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 214 ms, Memory: 53,628 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -3,19 +3,25 @@
 
 딕셔너리 사용
 '''
+#Refactoring
+# 행성의 숫자 단어와 순서를 매핑
+planet_numbers = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
+order_dict = {word: index for index, word in enumerate(planet_numbers)}
+
+
+# 입력된 문자열 리스트를 정렬하는 함수
+def sort_planet_numbers(words):
+    # 각 단어를 order_dict의 값(숫자)에 따라 정렬
+    sorted_words = sorted(words, key=lambda word: order_dict[word])
+    # 결과를 공백으로 연결하여 반환
+    return ' '.join(sorted_words)
+
+
+# 테스트 케이스 실행
 T = int(input())
 for test_case in range(1, T + 1):
-    tc, tc_len = map(str, input().split())
-    nums = list(map(str, input().split()))
-    planet_nums = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
-    planet_dict = dict(zip(planet_nums, range(len(planet_nums))))  # list to dict
-    value_list = []
-    for key in nums:
-        value_list.append(planet_dict[key])  # 키로 값에 접근해서 값만 저장
-    value_list = sorted(value_list)
-    # 리스트 인덱스에 맞게 출력
-    result = []
+    input()  # 테스트 케이스 번호와 길이를 받는 줄은 생략
+    nums = input().split()  # 숫자 문자열 리스트 입력
+    sorted_string = sort_planet_numbers(nums)
     print(f"#{test_case}")
-    for i in value_list:
-        print(f"{planet_nums[i]}", end=' ')
-    print()
+    print(sorted_string)
