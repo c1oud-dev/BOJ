4234f1db7454bc0e0941fb327e5a9fc373e57470
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-27 22:15:11 +0900

    [Silver V] Title: 카드1, Time: 60 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,12 +1,14 @@
+'''
+제일 위에 있는 카드를 바닥에 버린다.
+그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+버린 카드들을 순서대로 출력하고, 마지막에 남게 되는 카드 출력
+'''
 from collections import deque
-n=int(input())
-card=[]
-trash=[]
-for x in range(1,n+1):
-    card.append(x)
-card=deque(card)
-while card:
-    trash.append(card.popleft())
-    if len(card) != 0:
-        card.append(card.popleft())
-print(*trash)
+
+n = int(input())
+card_deq = deque(range(1, n + 1))  # n 만큼 큐에 넣기
+result = []
+while card_deq:
+    result.append(card_deq.popleft())
+    card_deq.rotate(-1)
+print(*result)
