89ae0f2b58debde7672b72ba9987ebbfa5374ab3
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-29 17:41:40 +0900

    [D3] Title: 퍼펙트 셔플, Time: 176 ms, Memory: 45,028 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -4,19 +4,27 @@
 N 개의 카드가 있는 덱이 주어질 때 이를 퍼펙트 셔플한 순서 출력
 홀수이면 교대로 놓을 때 먼저 놓는 쪽에 한 장이 더 들어가게 된다.
 '''
+from collections import deque
 import math
+
 T = int(input())  # 테스트 케이스 수
 for test_case in range(1, T + 1):
     N = int(input())  # 카드 수
-    card = list(map(str, input().split()))
-    first_half = [card[i] for i in range(math.ceil(N / 2))]  # 카드의 절반에서 앞 카드들만 저장
-    second_half = [card[i] for i in range(math.ceil(N / 2), N)]  # 카드의 절반에서 뒤 카드들만 저장
+    card = input().split()
+
+    # 절반으로 나눈 덱을 deque로 만들어 popleft()로 효율적인 제거 수행
+    first_half = deque(card[:math.ceil(N / 2)])
+    second_half = deque(card[math.ceil(N / 2):])
+
     result = []
-    while len(second_half) > 0:
-        result.append(first_half.pop(0))  # 첫 번째 원소를 빼서 결과에 넣기
-        result.append(second_half.pop(0))
-    # 홀수 일때 first 리스트는 하나의 카드가 남기 때문에 따로 추가
-    if N % 2 != 0:
-        result.append(first_half.pop())
-    print(f"#{test_case}", end=' ')
-    print(*result)
+    # 두 반쪽을 교차로 섞어 새로운 덱을 생성
+    while second_half:
+        result.append(first_half.popleft())
+        result.append(second_half.popleft())
+
+    # 홀수일 때 첫 번째 반쪽에 한 장 더 남아있는 경우 추가
+    if first_half:
+        result.append(first_half.popleft())
+
+    # 결과 출력
+    print(f"#{test_case} {' '.join(result)}")
