6e409410e13e7b7fa51c0bc084020360e5b40b89
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-30 20:59:22 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 114 ms, Memory: 45,388 KB -BaekjoonHub

--- SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.py
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.py
@@ -1,27 +1,21 @@
 '''
 I 이면 삽입, D이면 삭제
 '''
-#import re  # swea 사용 불가
 for test_case in range(1, 11):
-    _ = int(input())
-    origin = list(map(int, input().split()))
-    _ = int(input())
-    commands = input().replace("I", ";I").replace("D", ";D")  # I와 D를 기준으로 구분
-    # ";"로 나눠 명령어 분리
-    commands = commands.split(";")[1:]  # 첫 부분은 빈 문자열이므로 제외
+    _ = int(input())  # 원본 암호문 길이
+    origin = list(map(int, input().split()))  # 원본 암호문
+    _ = int(input())  # 명령어 개수
+    commands = input().replace("I", ";I").replace("D", ";D")  # I와 D로 구분하여 명령어 분리
+    commands = commands.split(";")[1:]  # 빈 첫 부분 제외
 
-    for i in commands:  # 첫 번째 인덱스는 공백이기 때문에 패스
-        command = list(map(str, i.split()))  # 명령어 따로 저장
+    for i in commands:
+        command = i.split()  # 명령어 분리
+        cmd_type, x, y = command[0], int(command[1]), int(command[2])
 
-        #int_command = list(map(int, command[1:]))  # str 이므로 int 형으로 다시 변환
-        # insert 와 delete 인걸 구분하려면 길이로 판단
-        if command[0] == "I":  # insert 연산
-            int_command = list(map(int, command[1:]))
-            insert_position = int_command[0]  # 삽입할 위치
-            insert_command = int_command[2:]  # 명령어 저장
-            origin[insert_position:insert_position] = insert_command
-        else:  #delete 연산
-            int_command = list(map(int, command[1:]))
-            del origin[int_command[0]:int_command[0] + int_command[1]]
+        if cmd_type == "I":  # insert 연산
+            s = map(int, command[3:])  # 삽입할 숫자들
+            origin[x:x] = s  # 리스트 슬라이싱으로 한번에 삽입
+        elif cmd_type == "D":  # delete 연산
+            del origin[x:x + y]  # 리스트 슬라이싱으로 삭제
 
     print(f"#{test_case} {' '.join(map(str, origin[:10]))}")
