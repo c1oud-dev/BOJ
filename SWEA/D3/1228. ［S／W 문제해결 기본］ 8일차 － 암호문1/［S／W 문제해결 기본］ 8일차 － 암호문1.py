48d4af04585470791d408a737c7992232c763277
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-10-30 18:12:41 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 113 ms, Memory: 44,260 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -2,15 +2,16 @@ for test_case in range(1, 11):
     _ = int(input())
     origin = list(map(int, input().split()))
     _ = int(input())
-    command = list(map(str, input().split("I")))  # I 구분자 기준으로 분리하여 저장
-    
-    for i in command:
-        command_list = list(map(int, i.split()))  # 명령어를 한 줄씩 꺼내서 다시 따로 저장
-        if len(command_list) == 0:  # 첫 번째 리스트는 비어있기 때문에 무시하는 코드 작성
-            continue
-        for j in range(command_list[1]):  
-            origin.insert(command_list[0] + j, command_list[j + 2])  # 원하는 위치 i 앞에 추가할 값 x를 삽입
-    
-    result = origin[:10]  # 10번째까지만 출력
+    command = input().split("I")[1:]  # 첫 번째 빈 요소를 제거하고 시작
 
-    print(f"#{test_case} {' '.join(map(str, result))}") 
+    for i in command:
+        command_list = list(map(int, i.split()))
+        insert_position = command_list[0]
+        num_to_insert = command_list[2:]
+        
+        # 해당 위치에 명령어대로 값 삽입
+        origin[insert_position:insert_position] = num_to_insert
+
+    # 10번째까지만 출력
+    result = origin[:10]
+    print(f"#{test_case} {' '.join(map(str, result))}")
