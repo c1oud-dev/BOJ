f555dd69aaeaa6aa27cfe033a83e1a8dfdb3df13
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-04 21:08:24 +0900

    [D3] Title: 늘어지는 소리 만들기, Time: 339 ms, Memory: 52,664 KB -BaekjoonHub

--- SWEA/D3/4676. 늘어지는 소리 만들기/늘어지는 소리 만들기.py
+++ SWEA/D3/4676. 늘어지는 소리 만들기/늘어지는 소리 만들기.py
@@ -4,16 +4,14 @@
 
 insert(i, x) 사용
 '''
-
 T = int(input())  # 테스트 케이스 수
 for test_case in range(1, T + 1):
     word = list(input().strip())  # 문자열
     h = int(input())  # 하이픈 개수
-    hyphen = sorted(list(map(int, input().split())))  # 하이픈 위치 정렬
+    hyphen = list(map(int, input().split()))  # 하이픈 위치
 
-    for i in range(h):
-        word.insert(hyphen[i], '-')
-        for j in range(i + 1, h):
-            hyphen[j] += 1
-            
+    # 하이픈 위치를 오른쪽에서 왼쪽 순서로 처리
+    for idx in sorted(hyphen, reverse=True):
+        word.insert(idx, '-')
+
     print(f"#{test_case} {''.join(word)}")
