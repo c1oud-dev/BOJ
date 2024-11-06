699c26e88c66a8e2d8fa185d7e2301e9b6abde24
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-06 16:25:11 +0900

    [level 1] Title: 모의고사, Time: 5.70 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,13 @@
 def correct(student_answer, answers):
-    cnt, j = 0, 0
-    for i in range(len(answers)):
-        if answers[i] == student_answer[j]:
-            cnt += 1
-        j += 1
-        if j >= len(student_answer):
-            j = 0
-
-    return cnt
+    return sum(1 for i, ans in enumerate(answers) if ans == student_answer[i % len(student_answer)])
 
 def solution(answers):
-    answer = []
-
-    one = correct([1, 2, 3, 4, 5], answers)
-    two = correct([2, 1, 2, 3, 2, 4, 2, 5], answers)
-    three = correct([3, 3, 1, 1, 2, 2, 4, 4, 5, 5], answers)
-    cnt = [one, two, three]
-    max_cnt = max(cnt)
-    for i in range(3):
-        if max_cnt == cnt[i]:
-            answer.append(i + 1)
-
-    return answer
+    patterns = [
+        [1, 2, 3, 4, 5], 
+        [2, 1, 2, 3, 2, 4, 2, 5], 
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
+    scores = [correct(pattern, answers) for pattern in patterns]
+    max_score = max(scores)
+    
+    return [i + 1 for i, score in enumerate(scores) if score == max_score]
