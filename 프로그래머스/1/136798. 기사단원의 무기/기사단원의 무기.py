3df8c9d7e8f95a71b1e048a2b7e96b1d8df008ab
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-09 15:16:04 +0900

    [level 1] Title: 기사단원의 무기, Time: 929.80 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,19 +1,15 @@
 def solution(number, limit, power):
     answer = 0
-
     for i in range(1, number + 1):
-        divisor_cnt = 0
-        # 약수 개수 계산을 1부터 sqrt(i)까지만 진행
-        for j in range(1, int(i**0.5) + 1):
-            if i % j == 0:
-                divisor_cnt += 1
-                if j != i // j:  # 제곱수가 아닌 경우에만 다른 쌍을 추가
-                    divisor_cnt += 1
-                
-            if divisor_cnt > limit:  # 제한 수치를 초과하면 중단
-                divisor_cnt = power
-                break
-
-        answer += divisor_cnt if divisor_cnt <= limit else power
-
+        cnt = 0  # 공격력 초기화
+        for j in range(1, int(i**0.5) + 1):  # 약수 확인
+            if i % j == 0:  # 약수
+                cnt += 1  # 약수 개수
+                if i // j != j:  # 몫과 나누는 수와 같다면 무시(6 x 6) 일 때
+                    cnt += 1
+        #무기 구매
+        if cnt > limit:
+            answer += power
+        else:
+            answer += cnt
     return answer
