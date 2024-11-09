e6904f1667b6b80a1f803fa0a5679e57264659ce
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-09 14:57:50 +0900

    [level 1] Title: 콜라 문제, Time: 0.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,7 +1,7 @@
 def solution(a, b, n):
     answer = 0
     while n >= a:
-        new_bottles = (n // a) * b
-        answer += new_bottles
-        n = new_bottles + (n % a)
+        bottles = (n // a) * b
+        answer += bottles
+        n = (n % a) + bottles
     return answer
