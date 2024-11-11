dd29179b0886062245ca6a86ece8533028152282
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-11 19:48:26 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,9 +1,2 @@
 def solution(n):
-    ans = 0
-    while n > 0:
-        if n % 2 != 0:
-            n -= 1  # jump
-            ans += 1
-        n //= 2
-
-    return ans
+    return bin(n).count('1')
