467bf20e3af544730d4d96c3a15354694f4c2cfd
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-12 14:02:52 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,13 +1,7 @@
 def solution(n, arr1, arr2):
     answer = []
-    # 배열 2진수로 변환, 한 줄 씩
     for i, j in zip(arr1, arr2):
-        bin_str = ''
-        for first, second in zip(format(i, f'0{n}b'), format(j, f'0{n}b')):
-            for f, s in zip(first, second):
-                if f == '1' or s == '1':
-                    bin_str += '#'
-                else:
-                    bin_str += ' '
+        # 두 값을 OR 연산한 후 n자리 2진수로 변환하여 #과 공백으로 매핑
+        bin_str = format(i | j, f'0{n}b').replace('1', '#').replace('0', ' ')
         answer.append(bin_str)
     return answer
