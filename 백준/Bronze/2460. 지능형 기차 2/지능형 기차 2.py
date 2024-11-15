e9586bcd69b6774a33071a32cbfc1b6056b0c6ed
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-15 16:34:12 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -1,15 +1,6 @@
-'''
- 타거나 내리는 사람 수를 자동으로 인식할 수 있는 장치가 있다.
- 사람이 가장 많을 때의 사람 수를 계산
-'''
-get_off, get_on = [], []
+result, max_on = 0, 0
 for _ in range(10):
     off, on = map(int, input().split())
-    get_off.append(off)
-    get_on.append(on)
-    
-result, max_on = 0, 0
-for off, on in zip(get_off, get_on):
     max_on -= off
     max_on += on
     result = max(result, max_on)
