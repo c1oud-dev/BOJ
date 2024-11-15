eaf6bc634570278ed5c0e14f783d86f5d14dacff
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-16 02:12:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 8.21 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,13 +1,11 @@
-import heapq
-
 def solution(k, score):
-    hall_of_fame = []  # 명예의 전당 (최소 힙)
-    result = []
+    answer = []
+    top = []
 
     for s in score:
-        heapq.heappush(hall_of_fame, s)  # 점수 추가
-        if len(hall_of_fame) > k:  # 크기 초과 시 최소값 제거
-            heapq.heappop(hall_of_fame)
-        result.append(hall_of_fame[0])  # 현재 명예의 전당의 최하위 점수 기록
+        top.append(s)
+        if len(top) > k:  # 명예의 전당 크기를 초과하면 최소값 제거
+            top.remove(min(top))
+        answer.append(min(top))  # 현재 명예의 전당의 최하위 점수 기록
 
-    return result
+    return answer
