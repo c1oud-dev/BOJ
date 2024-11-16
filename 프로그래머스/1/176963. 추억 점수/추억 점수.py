5a2f02e90051471cbe9a9f9fbd5ecb0c980ee26c
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2024-11-16 14:06:10 +0900

    [level 1] Title: 추억 점수, Time: 1.19 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,9 +1,11 @@
 def solution(name, yearning, photo):
+    # 이름과 그리움 점수를 딕셔너리로 매핑
+    yearning_dict = dict(zip(name, yearning))
+    
     answer = []
-    for i in photo:
-        score = 0
-        for j in i:
-            if j in name:  # 해당 이름이 있는 경우
-                score += yearning[name.index(j)]
+    for people in photo:
+        # 각 사진 속 인물의 점수를 계산
+        score = sum(yearning_dict.get(person, 0) for person in people)
         answer.append(score)
+    
     return answer
